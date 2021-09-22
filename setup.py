f090b7b6814778eb8a9cdd0a2986aadc01d9559d
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2021-09-22 18:35:42 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import re
+
+version = "0.0.0.1a"
+
+if version.endswith(('a', 'b', 'rc')):
+    # append version identifier based on commit count
+    try:
+        import subprocess
+        p = subprocess.Popen(['git', 'rev-list', '--count', 'HEAD'],
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        if out:
+            version += out.decode('utf-8').strip()
+        p = subprocess.Popen(['git', 'rev-parse', '--short', 'HEAD'],
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        if out:
+            version += '+g' + out.decode('utf-8').strip()
+    except Exception:
+        pass
+
+setup(
+    name="PyRist",
+    version=version,
+    description="A module for compiling my lang",
+    license="CC0-1.0",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+    ],
+    python_requires=">=3.8",
+    keywords="discord discord-py discord-bot utils utility",
+    packages=["ristpy"]
+)
