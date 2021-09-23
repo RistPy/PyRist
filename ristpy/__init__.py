fb01210625ab392d0977ef961a92462e8863ad26
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2021-09-23 08:36:22 +0530

    Update __init__.py

--- ristpy/__init__.py
+++ ristpy/__init__.py
@@ -5,13 +5,19 @@ def _replace(code: str, key: str, value: str = "") -> str:
     return code
 
 def _replaceall(code: str) -> str:
+    code = _replace(code, "+@", "from")
+    code = _replace(code, "@+", "import")
     code = _replace(code, "{", "(")
     code = _replace(code,"}", ")")
     code = _replace(code, ";", "")
     return code
 
-def rist(fp: str) -> str:
-    with open(fp, 'r') as f:
-        code = f.read()
+def rist(arg: str, fp: bool = True) -> str:
+    if fp:
+        with open(arg, 'r') as f:
+            code = f.read()
+    else:
+        code = arg
+
     code = _replaceall(code)
     return code
