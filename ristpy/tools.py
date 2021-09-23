57f87e3b19c8acfb8cc00b3a416dd2479f292a20
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2021-09-23 10:04:22 +0530

    Create tools.py

---
+++ ristpy/tools.py
@@ -0,0 +1,20 @@
+class Sender:
+    __slots__ = ('iterator', 'send_value')
+    def __init__(self, iterator):
+        self.iterator = iterator
+        self.send_value = None
+
+    def __iter__(self):
+        return self._internal(self.iterator.__iter__())
+
+    def _internal(self, base):
+        try:
+            while True:
+                value = base.asend(self.send_value)
+                self.send_value = None
+                yield self.set_send_value, value
+        except StopIteration:
+            pass
+
+    def set_send_value(self, value):
+        self.send_value = value
