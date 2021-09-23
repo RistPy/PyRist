102b46700863d970a8e892f3fa6efec91bfba20f
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2021-09-23 11:07:11 +0530

    Create __main__.py

---
+++ ristpy/__main__.py
@@ -0,0 +1,22 @@
+import argparse
+
+from ristpy import rist, execute
+
+def compile_and_run(fp: str):
+    if not fp.endswith(".rist"):
+        raise TypeError("You must provide file with extension '.rist'")
+    code = rist(fp)
+    execute(code)
+
+def parse_args():
+    parser = argparse.ArgumentParser(prog='rist', description='Rist Lang')
+    parser.add_argument('file', type=str, help='compiles rist lang to python and executes it.')
+    parser.set_defaults(func=compile_and_run)
+    return parser, parser.parse_args()
+
+def main():
+    parser, args = parse_args()
+    args.func(parser, args)
+
+if __name__ == "__main__":
+    main()
