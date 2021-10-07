40b3f31f750a38a45b62645192f01adccc786e36
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2021-10-07 12:13:58 +0530

    Update __init__.py

--- ristpy/__init__.py
+++ ristpy/__init__.py
@@ -9,11 +9,12 @@ from .builtins import *
 __all__ = ("rist", "execute")
 
 class __CompiledCode:
-  def __init__(self, code: str) -> None:
+  def __init__(self, code: str, fname: str = '<rist-executor>`) -> None:
     self.__code = code
+    self.file = fname
 
   def __repr__(self) -> str:
-    return "<CompiledCode length={0} from=rist to=python>".format(len(self.code))
+    return str(self)
 
   def __str__(self) -> str:
     return self.__code
@@ -68,8 +69,10 @@ def rist(arg: str, fp: bool = True) -> str:
   if fp:
     with open(arg, 'r') as f:
       code = f.read()
+    fname = arg
   else:
     code = arg
+    fname = None
   lines = code.splitlines()
   nlines = []
   for index, line in enumerate(lines):
@@ -81,13 +84,12 @@ def rist(arg: str, fp: bool = True) -> str:
       raise SyntaxError(f'invalid syntax\nline {index+1}\nevery line should end with ";"')
     nlines.append(line.rstrip(";"))
   code = "\n".join(list(line for line in nlines))
-  return __CompiledCode(__compileall(code))
+  return __CompiledCode(__compileall(code), fname)
 
 def execute(code: __CompiledCode) -> None:
   if not isinstance(code, __CompiledCode):
     raise TypeError("The code must be compiled from ristpy module not any other")
-  code = str(code)
-  for send, result in Sender(CodeExecutor(code, arg_dict=get_builtins())):
+  for send, result in Sender(CodeExecutor(str(code), arg_dict=get_builtins(), fname=code.file)):
     if result is None:
       continue
     send(result)
