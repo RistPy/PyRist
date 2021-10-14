be1d2b2e43b1003753ef50adc340a51be33b1116
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2021-10-14 10:18:35 +0530

    Update __init__.py

--- ristpy/__init__.py
+++ ristpy/__init__.py
@@ -1,4 +1,8 @@
+import re
 import secrets
+
+from collections import OrderedDict
+from typing import Union, List, Generator, Tuple
 
 from .tools import *
 from .scope import *
@@ -9,7 +13,7 @@ from .builtins import *
 __all__ = ("rist", "execute")
 
 class __CompiledCode:
-  def __init__(self, code: str, fname: str = '<rist-executor>') -> None:
+  def __init__(self, code: str, fname: str = '<unknown>') -> None:
     self.__code = code
     self.file = fname
 
@@ -23,56 +27,137 @@ class __CompiledCode:
   def code(self) -> str:
     return self.__code
 
-def __replace(code: str, key: str, value: str = "") -> str:
-    while key in code:
-        code = code.replace(key, value)
-    return code
+class __Token:
+  def __init__(
+    self,
+    name: str,
+    value: Union[str, int],
+    line: int,
+    coloumn: int
+  ) -> None:
+    self.name = name
+    self.value = str(value)
+    self.line = line
+    self.coloumn = coloumn
 
-def __interprete_imports(line: str) -> str:
-  if line.startswith("+@ ") and " @+ " in line:
-    return __replace(__replace(line, "+@", "from"), "@+", "import")
-  if line.startswith("@+ "):
-    return __replace(line, "@+", "import")
-  return line
+  def __repr__(self) -> str:
+    return "<Token name='{0.name}' value='{0.value}' line={0.line} coloumn={0.coloumn}>".format(
+      self
+    )
 
-def __interprete_comments(line: str) -> str:
-  h = secrets.token_urlsafe(50)
-  line = __replace(line, '#', h)
-  line = __replace(__replace(line, '//', '#'), h, "//")
-  return line
+  def __str__(self) -> str:
+    return str(self.value)
 
-def __interpreteall(code: str) -> str:
-  lines = code.splitlines()
-  nlines = []
-  for line in lines:
-    line.rstrip("\n")
-    line = __interprete_imports(line)
-    line = __interprete_comments(line)
-    nlines.append(line)
-  return "\n".join(list(line for line in nlines))
+class __Interpreter:
+  __rules = [
+        ('COMMENT', r'//.*'),
+        ('STRING', r'"(\\"|[^"])*"'),
+        ('STRING', r"'(\\'|[^'])*'"),
+        ('NUMBER', r'\d+\.\d+'),
+        ('NUMBER', r'\d+'),
+        ('ARROW', r'\} \=\-\=\> '),
+        ('FUNCDEF', 'define'),
+        ('NAME', r'[a-zA-Z_]\w*|[a-zA-Z0-9_]\w*'),
+        ('TABSPACE', '\t'),
+        ('SPACE', ' '),
+        ('OPERATOR', r'[\+\*\-\/%]'),       # arithmetic operators
+        ('OPERATOR', r'<=|>=|==|!=|<|>'),   # comparison operators
+        ('OPERATOR', r'\|\||&&'),           # boolean operators
+        ('OPERATOR', r'\.\.\.|\.\.'),       # range operators
+        ('OPERATOR', '!'),                  # unary operator
+        ('ASSIGN', '='),
+        ('LPAREN', r'\('),
+        ('RPAREN', r'\)'),
+        ('LBRACK', r'\['),
+        ('RBRACK', r'\]'),
+        ('LCBRACK', '{'),
+        ('RCBRACK', '}'),
+        ('COLON', ':'),
+        ('COMMA', ','),
+        ('DOT', '.'),
+        ('FROM', r'\+@'),
+        ('IMPORT', r'@\+'),
+        ('LARROW', r'\<'),
+        ('RARROW', r'\>')
+  ]
 
-def __compileall(code: str) -> str:
-  d1 = secrets.token_urlsafe(50)
-  d2 = secrets.token_urlsafe(50)
-  code = __interpreteall(code)
-  code = __replace(__replace(code, "(", d1), ")", d2)
-  code = __replace(code, "} =-> ", "} → ")
-  code = __replace(__replace(code, "}", ")"), "{", "(")
-  code = __replace(__replace(code, ">", "}"), "<", "{")
-  code = __replace(code, "} → ", "} -> ")
-  code = __replace(code, ") → ", ") -> ")
-  code = __replace(__replace(code, d1, "{"), d2, "}")
-  code = __replace(code, "define", "def")
-  return code
+  def __init__(self) -> None:
+    self.__regex = self.__compile_rules(self.__rules)
 
-def rist(arg: str, fp: bool = True) -> str:
+  def __convert_rules(self, rules: List[Tuple[str, str]]) -> Generator[str]:
+        grouped_rules = OrderedDict()
+        for name, pattern in rules:
+            grouped_rules.setdefault(name, [])
+            grouped_rules[name].append(pattern)
+
+        for name, patterns in iter(grouped_rules.items()):
+            joined_patterns = '|'.join(['({})'.format(p) for p in patterns])
+            yield '(?P<{}>{})'.format(name, joined_patterns)
+
+  def __compile_rules(self, rules):
+    return re.compile('|'.join(self.__convert_rules(rules)))
+
+  def __interprete_line(self, line, line_num) -> Generator[__Token]:
+    pos = 0
+    while pos < len(line):
+      matches = self.__regex.match(line, pos)
+      if matches is not None:
+         name = matches.lastgroup
+         pos = matches.end(name)
+         value = matches.group(name)
+         if name == "TABSPACE":
+           value = "	"
+         elif name == "SPACE":
+           value = " "
+         yield __Token(name, value, line_num, matches.start() + 1)
+
+  def interprete(self, s) -> str:
+    tokens = []
+    line_num = 0
+    for line_num, line in enumerate(s.splitlines(), 1):
+      line = line.rstrip()
+      if not line:
+        tokens.append(Token('NEWLINE', "\n", line_num, 1)
+        continue
+      line_tokens = list(self._tokenize_line(line, line_num))
+      if line_tokens:
+        tokens.extend(line_tokens)
+        tokens.append(Token('NEWLINE', "\n", line_num, len(line) + 1))
+
+    ntoks = []
+    for tok in tokens:
+      if tok.name == "LCBRACK" and tok.value == "{":
+        ntoks.append(__Token("LPAREN", "(", tok.line, tok.coloumn))
+      elif tok.name == "RCBRACK" and tok.value == "}":
+        ntoks.append(__Token("RPAREN", ")", tok.line, tok.coloumn))
+      elif tok.name == "COMMENT" and tok.value.startswith('//'):
+        ntoks.append(__Token("COMMENT", ("#" + tok.value[2:]), tok.line, tok.coloumn))
+      elif tok.name == "FUNCDEF" and tok.value == "define":
+        ntoks.append(__Token("FUNCDEF", "def", tok.line, tok.coloumn))
+      elif (tok.name == "LPAREN" and tok.value == "(") or (tok.name == "LARROW" and tok.value == "<"):
+        ntoks.append(__Token("LCBRACK", "{", tok.line, tok.coloumn))
+      elif (tok.name == "RPAREN" and tok.value == ")") or (tok.name == "RARROW" and tok.value == ">"):
+        ntoks.append(__Token("RCBRACK", "}", tok.line, tok.colomn))
+      elif tok.name == "ARROW" and tok.value == "} =-=> ":
+        ntoks.append(__Token(tok.name, ") -> ", tok.line, tok.coloumn))
+      elif tok.name == "FROM" and tok.value == "+@":
+        ntoks.append(__Token(tok.name, "from", tok.line, tok.coloumn))
+      elif tok.name == "IMPORT" and tok.value == "@+"
+        ntoks.append(__Token(tok.name, "import", tok.line, tok.coloumn))
+      else:
+        ntoks.append(tok)
+
+    return "".join(list(str(t) for t in ntoks))
+
+
+def rist(arg: str, fp: bool = True) -> __CompiledCode:
   if fp:
     with open(arg, 'r') as f:
       code = f.read()
     fname = arg
   else:
     code = arg
-    fname = None
+    fname = '<unknown>'
   lines = code.splitlines()
   nlines = []
   for index, line in enumerate(lines):
@@ -84,7 +169,7 @@ def rist(arg: str, fp: bool = True) -> str:
       raise SyntaxError(f'invalid syntax\nline {index+1}\nevery line should end with ";"')
     nlines.append(line.rstrip(";"))
   code = "\n".join(list(line for line in nlines))
-  return __CompiledCode(__compileall(code), fname)
+  return __CompiledCode(__Interpreter().interprete, fname)
 
 def execute(code: __CompiledCode) -> None:
   if not isinstance(code, __CompiledCode):
