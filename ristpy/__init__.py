d63fb5f11c32d5a367fe69332118c5af1ba4adf7
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2022-12-15 18:52:11 +0530

    Added import exceptions..

--- ristpy/__init__.py
+++ ristpy/__init__.py
@@ -273,9 +273,9 @@ class __Interpreter:
         ('COMMENT', r'#.*'),
         ('STRING', r'((".*?")(?<!(\\)))'),
         ('STRING', r"(('.*?')(?<!(\\)))"),
-        ('FROM', r'^(\\s)?\+@ ({ATTRIBUTED_NAME}|{NAME}) {ERR_IMPORT}'),
+        ('FROM', r'^(\\s)?\+@ ({ATTRIBUTED_NAME}|{NAME}) @\+ {ATTRIBUTED_NAME}'),
         ('IMPORT', r'^(\\s)?@\+ {ATTRIBUTED_NAME}'),
-        ('ERR_IMPORT', r'\+@ ({ATTRIBUTED_NAME}|{NAME}) {ERR_IMPORT}'),
+        ('ERR_IMPORT', r'\+@ ({ATTRIBUTED_NAME}|{NAME}) @\+ {ATTRIBUTED_NAME}'),
         ('ERR_IMPORT', r'@\+ {ATTRIBUTED_NAME}'),
         ('PREDEF', r'\$(i|p|d|l|t|n|m|s|u)'),
         ('AT', '@{ATTRIBUTED_NAME}'),
@@ -355,6 +355,7 @@ class __Interpreter:
             kwrds = dict(filename=f, lineno=line_num, offset=pos+1, text=line)
             for k, v in kwrds.items():
               setattr(err, k, v)
+            if sys.version_info>(3,9):setattr(err, end_offset, pos+1+len(value))
             raise err
 
           tokens.append(_Token(name, value, line_num, matches.start() + 1))
