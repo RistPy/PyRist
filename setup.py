b80f42068ad79788b630d2b348c6fa7b8281bb05
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2022-12-31 16:37:42 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,39 +1,46 @@
 from setuptools import setup, find_packages
 
+
 version = "1.0"
 
+with open("README.md", "r") as f:
+  long_description = f.read()
+
 if version.endswith(('a', 'b', 'rc')):
-    # append version identifier based on commit count
-    try:
-        import subprocess
-        p = subprocess.Popen(['git', 'rev-list', '--count', 'HEAD'],
-                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        out, err = p.communicate()
-        if out:
-            version += out.decode('utf-8').strip()
-        p = subprocess.Popen(['git', 'rev-parse', '--short', 'HEAD'],
-                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        out, err = p.communicate()
-        if out:
-            version += '+g' + out.decode('utf-8').strip()
-    except Exception:
-        pass
+  # append version identifier based on commit count
+  try:
+    import subprocess
+    p = subprocess.Popen(['git', 'rev-list', '--count', 'HEAD'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+    if out:
+      version += out.decode('utf-8').strip()
+    p = subprocess.Popen(['git', 'rev-parse', '--short', 'HEAD'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+    if out:
+      version += '+g' + out.decode('utf-8').strip()
+  except Exception:
+    pass
 
 setup(
-    name="RistLang",
-    version=version,
-    description="A module for compiling my lang",
-    license="MIT",
-    python_requires=">=3.8",
-    entry_points={'console_scripts': ['rist=ristpy.__main__:main']},
-    install_requires=["import_expression"],
-    packages=find_packages(),
-    classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+  name="RistLang",
+  version=version,
+  description="A module for compiling RistLang",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  license="MIT",
+  python_requires=">=3.8",
+  entry_points={'console_scripts': ['rist=ristpy.__main__:main']},
+  install_requires=["import_expression"],
+  packages=find_packages(),
+  classifiers=[
+    "Programming Language :: Rist",
+    "Programming Language :: Rist :: 1.0",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
     ],
 )
